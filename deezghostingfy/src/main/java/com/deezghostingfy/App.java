dc55858a96b0dc2f9c5762df8315fd2ef8adedfc
Davi James <davi.vilela9009@gmail.com>
2023-11-27 12:22:34 -0300

    Comeco da integracao com o banco de dados

--- deezghostingfy/src/main/java/com/deezghostingfy/App.java
+++ deezghostingfy/src/main/java/com/deezghostingfy/App.java
@@ -7,14 +7,28 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 
 import com.deezghostingfy.dados.Sessao;
 import com.deezghostingfy.pesquisa.Pesquisa;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
+
 
     private static Scene scene;
     private static Stage estagio;
@@ -42,7 +56,49 @@ public class App extends Application {
     }
 
     public static void main(String[] args) throws IOException {
-        launch();
+        ArrayList<Teste> array = new ArrayList<>();
+        Teste t1 = new Teste();
+        t1.setId(1);
+        t1.setDescricao("aaaaaa");
+        t1.setPreco(213);
+        array.add(t1);
+
+        MongoClientURI connectionString = new MongoClientURI("mongodb://localhost:27017");
+        MongoClient mongoClient = new MongoClient(connectionString);
+        CodecRegistry pojoCodecRegistry = org.bson.codecs.configuration.CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), org.bson.codecs.configuration.CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+        MongoDatabase database = mongoClient.getDatabase("mongodbjava").withCodecRegistry(pojoCodecRegistry);  
+
+        //MongoClient mongoClient = new MongoClient( "localhost" , 27017 ); 
+        //MongoDatabase db = mongoClient.getDatabase("mongodbjava");
+
+		//Inserting sample record by creating collection and document.
+		//MongoCollection<org.bson.Document>  collection= database.getCollection("javaprogram");
+		//org.bson.Document doc =(org.bson.Document) new org.bson.Document("teste1", array);
+        
+        MongoCollection<Teste>  collection= database.getCollection("javaprogram", Teste.class);
+		collection.drop(); 
+
+        
+        //doc.append("campo", "sexo");
+		collection.insertOne(t1);
+        //collection.insertOne(t1);
+        //doc =(org.bson.Document) new org.bson.Document("name2","hello");
+		//collection.insertOne(doc);
+        //doc =(org.bson.Document) new org.bson.Document("name3","sexo");
+        //collection.insertOne(doc);
+
+        System.out.println("---------------------------------------");
+        for(Teste a: collection.find()) {
+            System.out.println(a.getDescricao());
+        }
+       
+
+        //Atualizando um objeto
+//        collection.updateOne(new Document("_id", 1), set("descricao", "Arroz Parbolizado"));
+
+        //Deletando um objeto
+//        collection.deleteOne(new Document("descricao", "  Parbolizado"));
+        //launch();
     }
 
 }
--- deezghostingfy/src/main/java/module-info.java
+++ deezghostingfy/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.deezghostingfy {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires mongo.java.driver;
 
     opens com.deezghostingfy.controlefxml to javafx.fxml;
     exports com.deezghostingfy;
